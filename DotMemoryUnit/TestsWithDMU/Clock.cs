6f3956d5093cc934172c06c5f5bd04e2956fc768
Maarten Balliauw <maarten@maartenballiauw.be>
2024-06-06 13:57:33 +0200

    To file-scoped namespace

--- DotMemoryUnit/TestsWithDMU/Clock.cs
+++ DotMemoryUnit/TestsWithDMU/Clock.cs
@@ -1,18 +1,17 @@
 ﻿using System;
 using System.Timers;
 
-namespace ExampleTestsWithDMU
-{
-    class Clock
-        : IDisposable
-    {
-        public void OnTick(object state, ElapsedEventArgs args)
-        {
-            Console.WriteLine(DateTime.UtcNow);
-        }
+namespace TestsWithDMU;
 
-        public void Dispose()
-        {
-        }
+class Clock
+    : IDisposable
+{
+    public void OnTick(object state, ElapsedEventArgs args)
+    {
+        Console.WriteLine(DateTime.UtcNow);
+    }
+
+    public void Dispose()
+    {
     }
 }
--- DotMemoryUnit/TestsWithDMU/ClockFacts.cs
+++ DotMemoryUnit/TestsWithDMU/ClockFacts.cs
@@ -1,79 +1,77 @@
 ﻿using System;
 using System.Threading;
-using ExampleTestsWithDMU;
 using JetBrains.dotMemoryUnit;
 using Xunit;
 using Xunit.Abstractions;
 using Timer = System.Timers.Timer;
 
-namespace TestsWithDMU
+namespace TestsWithDMU;
+
+public class ClockFacts
 {
-    public class ClockFacts
+    public ClockFacts(ITestOutputHelper outputHelper)
     {
-        public ClockFacts(ITestOutputHelper outputHelper)
+        DotMemoryUnitTestOutput.SetOutputMethod(
+            message => outputHelper.WriteLine(message));
+    }
+
+    [Fact]
+    public void ClockDisposesCorrectly()
+    {
+        var isolator = new Action(() =>
         {
-            DotMemoryUnitTestOutput.SetOutputMethod(
-                message => outputHelper.WriteLine(message));
-        }
+            // Arrange
+            Timer timer;
+            using (Clock clock = new Clock())
+            {
+                // Act
+                timer = new Timer(1000);
+                timer.Elapsed += clock.OnTick;
+                timer.Start();
 
-        [Fact]
-        public void ClockDisposesCorrectly()
+                Thread.Sleep(5 * 1000); // Run clock for 5 seconds
+            }
+        });
+
+        isolator();
+
+        // Run explicit GC
+        GC.Collect();
+
+        // Assert
+        dotMemory.Check(memory => 
+            Assert.Equal(0, memory.GetObjects(where => where.Type.Is<Clock>()).ObjectsCount));
+    }
+
+    [Fact]
+    public void ClockDisposesCorrectlyFixed()
+    {
+        var isolator = new Action(() =>
         {
-            var isolator = new Action(() =>
+            // Arrange
+            using (Clock clock = new Clock())
+            using (Timer timer = new Timer(1000))
             {
-                // Arrange
-                Timer timer;
-                using (Clock clock = new Clock())
-                {
-                    // Act
-                    timer = new Timer(1000);
-                    timer.Elapsed += clock.OnTick;
-                    timer.Start();
+                // Act
+                timer.Elapsed += clock.OnTick;
+                timer.Start();
 
-                    Thread.Sleep(5 * 1000); // Run clock for 5 seconds
-                }
-            });
+                Thread.Sleep(5 * 1000); // Run clock for 5 seconds
 
-            isolator();
+                timer.Stop();
+                timer.Elapsed -= clock.OnTick;
+            }
+        });
 
-            // Run explicit GC
-            GC.Collect();
+        isolator();
 
-            // Assert
-            dotMemory.Check(memory => 
-                Assert.Equal(0, memory.GetObjects(where => where.Type.Is<Clock>()).ObjectsCount));
-        }
+        // Run explicit GC
+        GC.Collect();
 
-        [Fact]
-        public void ClockDisposesCorrectlyFixed()
+        // Assert
+        dotMemory.Check(memory =>
         {
-            var isolator = new Action(() =>
-            {
-                // Arrange
-                using (Clock clock = new Clock())
-                using (Timer timer = new Timer(1000))
-                {
-                    // Act
-                    timer.Elapsed += clock.OnTick;
-                    timer.Start();
-
-                    Thread.Sleep(5 * 1000); // Run clock for 5 seconds
-
-                    timer.Stop();
-                    timer.Elapsed -= clock.OnTick;
-                }
-            });
-
-            isolator();
-
-            // Run explicit GC
-            GC.Collect();
-
-            // Assert
-            dotMemory.Check(memory =>
-            {
-                Assert.Equal(0, memory.GetObjects(where => where.Type.Is<Clock>()).ObjectsCount);
-            });
-        }
+            Assert.Equal(0, memory.GetObjects(where => where.Type.Is<Clock>()).ObjectsCount);
+        });
     }
 }
